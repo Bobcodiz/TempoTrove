db6bbdec46a93189282972b3fee57273f819fe0f
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-10 20:06:16 +0300

    ...

---
+++ TempoTrove/src/main/java/com/codiz/TempoTrove/TempoTroveApplication.java
@@ -0,0 +1,13 @@
+package com.codiz.TempoTrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoTroveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoTroveApplication.class, args);
+	}
+
+}
---
+++ TempoTrove/src/test/java/com/codiz/TempoTrove/TempoTroveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codiz.TempoTrove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempoTroveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
