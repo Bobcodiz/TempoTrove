814a6576ba214988c37d81b296ec0202dc7b9be7
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-10 20:27:41 +0300

    ...

--- TempoTrove/src/main/java/com/codiz/TempoTrove/repository/UserResourceRepository.java
+++ TempoTrove/src/main/java/com/codiz/TempoTrove/repository/UserResourceRepository.java
@@ -1,4 +1,9 @@
 package com.codiz.TempoTrove.repository;
 
-public class UserResourceRepository {
+import com.codiz.TempoTrove.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserResourceRepository extends JpaRepository<UserModel, Long> {
 }
